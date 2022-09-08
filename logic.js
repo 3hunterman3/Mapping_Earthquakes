5c009f83d0dffaa36d2881d515f5c8bd140d86e6
3hunterman3 <yjk435@nyu.edu>
2022-09-07 21:08:25 -0400

    new info

--- logic.js
+++ logic.js
@@ -1,79 +1,23 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
- // Create the map object with center at the San Francisco airport.
- let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+// Create the map object with center and zoom level.
+let map = L.map('mapid').setView([30, 30], 2);
 
-// We create the tile layer that will be the background of our map.
-// We create the tile layer that will be the background of our map.
+// Accessing the airport GeoJSON URL
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: API_KEY
+ attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+ maxZoom: 18,
+ accessToken: API_KEY
 });
 
-// Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
-//  Add a marker to the map for Los Angeles, California.
-let marker = L.circle([34.0522, -118.2437], {
-    radius: 300,
-    color: "orange",
-    fillColor: '#ffffa1'
- }).addTo(map);
+let airportData = "https://raw.githubusercontent.com/<3hunterman3>/Mapping_Earthquakes/main/majorAirports.json";
 
- // An array containing each city's location, state, and population.
-let cities = [{
-    location: [40.7128, -74.0059],
-    city: "New York City",
-    state: "NY",
-    population: 8398748
-  },
-  {
-    location: [41.8781, -87.6298],
-    city: "Chicago",
-    state: "IL",
-    population: 2705994
-  },
-  {
-    location: [29.7604, -95.3698],
-    city: "Houston",
-    state: "TX",
-    population: 2325502
-  },
-  {
-    location: [34.0522, -118.2437],
-    city: "Los Angeles",
-    state: "CA",
-    population: 3990456
-  },
-  {
-    location: [33.4484, -112.0740],
-    city: "Phoenix",
-    state: "AZ",
-    population: 1660272
-  }
-  ];
-
-// // looping through the cities array
-// cities.forEach(function(city){
-//     console.log(city)
-//     L.marker(city.location).addTo(map)
-// });
-
-// Get data from cities.js
-let cityData = cities;
-
-// Coordinates for each point to be used in the polyline.
-let line = [
-    [33.9416, -118.4085],
-    [37.6213, -122.3790],
-    [40.7899, -111.9791],
-    [47.4502, -122.3088]
-  ];
-
-// Create a polyline using the line coordinates and make the line yellow.
-L.polyline(line, {
-    color: "yellow"
- }).addTo(map);
-
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJSON(data).addTo(map);
+});

8b4f500e9084f8282bc681bbcff6c73384fcc6bb
3hunterman3 <yjk435@nyu.edu>
2022-09-08 13:10:54 -0400

    new files added

--- logic.js
+++ logic.js
@@ -1,11 +1,8 @@
-// Add console.log to check to see if our code is working.
-console.log("working");
-
 // Create the map object with center and zoom level.
 let map = L.map('mapid').setView([30, 30], 2);
 
 // Accessing the airport GeoJSON URL
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
  maxZoom: 18,
  accessToken: API_KEY
@@ -19,5 +16,5 @@ let airportData = "https://raw.githubusercontent.com/<3hunterman3>/Mapping_Earth
 d3.json(airportData).then(function(data) {
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data).addTo(map);
+  L.geoJSON(data).addTo(map);
 });
